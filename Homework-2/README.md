38e621d1e3ee1fdff4cae5d9bc863edd28acc115
Ezgi Oflar <ezgioflar@gmail.com>
2023-03-06 23:29:55 +0300

    Homework 2 commit.

--- Homework-2/README.md
+++ Homework-2/README.md
@@ -1,1 +1,2 @@
-Readme
+# UpSchool 2. homework
+
---
+++ Homework-2/src/UpSchool.Wasm/Common/Utilities/Undo.cs
@@ -0,0 +1,30 @@
+﻿namespace UpSchool.Wasm.Common.Utilities
+{
+    class Undo
+    {
+        public string Password { get; set; }
+
+        //T anında nesneyi tutacak olan metod.
+        public UndoMemento Kaydet()
+        {
+            return new UndoMemento
+            {
+                Password = this.Password,
+            };
+        }
+
+        //T anındaki nesneye bizi ulaşturacak olan metod.
+        public void OncekiniYukle(UndoMemento Memento)
+        {
+            this.Password = Memento.Password;
+        }
+    }
+    class UndoMemento
+    {
+        public string Password { get; set; }
+    }
+    class UndoCareTaker
+    {
+        public UndoMemento Memento { get; set; }
+    }
+}
